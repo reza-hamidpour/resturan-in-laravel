f43c541032689c2cf5fec7f334d00f0e2445af80
R. Hamidpour <hamidpourr@gmail.com>
2022-01-27 14:12:13 +0330

    Impelimenting home page.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,290 @@
+@extends('layouts/base')
+@section('content')
+    <!-- ##### Instagram Area Start ##### -->
+    <ul class="nav nav-tabs justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active" id="menu-irani-tab" data-bs-toggle="tab" data-bs-target="#menu-irani" type="button" role="tabpanel" aria-controls="menu-irani" aria-selected="true" aria-current="page" href="#">منو ایرانی</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="menu-farangi-tab" data-bs-toggle="tab" data-bs-target="#menu-farangi" type="button" role="tabpanel" aria-controls="menu-farangi" aria-selected="true" href="#">منو فرنگی</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="menu-italy-tab" data-bs-toggle="tab" data-bs-target="#menu-farangi" type="button" role="tabpanel" aria-controls="menu-farangi" aria-selected="true" href="#">منو ایتالیایی</a>
+        </li>
+    </ul>
+    <div class="tab-pane fade show active" id="menu-irani" role="tabpanel" aria-labelledby="menu-irani-tab">
+        <div class="menu-area-resturan d-flex flex-wrap">
+        <!-- Single Instagram -->
+            <div class="single-instagram">
+                <div class="content-food">
+                    <img src="img/bg-img/insta1.jpg" alt="">
+                    <!-- Image Zoom -->
+                    <a href="img/bg-img/insta1.jpg" class="img-zoom" title="پست اینستاگرام">+</a>
+                </div>
+                <div class="label-food">
+                    <p class="label-food-name">قرمه سبزی</p>
+                    <p class="label-food-price">قیمت: ۱۳۸،۰۰۰ تومان</p>
+                </div>
+            </div>
+
+
+
+        <!-- Single Instagram -->
+        <div class="single-instagram">
+            <img src="img/bg-img/insta2.jpg" alt="">
+            <!-- Image Zoom -->
+            <a href="img/bg-img/insta2.jpg" class="img-zoom" title="پست اینستاگرام">+</a>
+            <div class="label-food">
+                <p class="label-food-name">قرمه سبزی</p>
+                <p class="label-food-price">قیمت: ۱۳۸،۰۰۰ تومان</p>
+            </div>
+        </div>
+
+        <!-- Single Instagram -->
+        <div class="single-instagram">
+            <img src="img/bg-img/insta3.jpg" alt="">
+            <!-- Image Zoom -->
+            <a href="img/bg-img/insta3.jpg" class="img-zoom" title="پست اینستاگرام">+</a>
+            <div class="label-food">
+                <p class="label-food-name">قرمه سبزی</p>
+                <p class="label-food-price">قیمت: ۱۳۸،۰۰۰ تومان</p>
+            </div>
+        </div>
+
+        <!-- Single Instagram -->
+        <div class="single-instagram">
+            <img src="img/bg-img/insta4.jpg" alt="">
+            <!-- Image Zoom -->
+            <a href="img/bg-img/insta4.jpg" class="img-zoom" title="پست اینستاگرام">+</a>
+            <div class="label-food">
+                <p class="label-food-name">قرمه سبزی</p>
+                <p class="label-food-price">قیمت: ۱۳۸،۰۰۰ تومان</p>
+            </div>
+        </div>
+
+        <!-- Single Instagram -->
+        <div class="single-instagram">
+            <img src="img/bg-img/insta5.jpg" alt="">
+            <!-- Image Zoom -->
+            <a href="img/bg-img/insta5.jpg" class="img-zoom" title="پست اینستاگرام">+</a>
+            <div class="label-food">
+                <p class="label-food-name">قرمه سبزی</p>
+                <p class="label-food-price">قیمت: ۱۳۸،۰۰۰ تومان</p>
+            </div>
+        </div>
+
+        <!-- Single Instagram -->
+        <div class="single-instagram">
+            <img src="img/bg-img/insta6.jpg" alt="">
+            <!-- Image Zoom -->
+            <a href="img/bg-img/insta6.jpg" class="img-zoom" title="پست اینستاگرام">+</a>
+            <div class="label-food">
+                <p class="label-food-name">قرمه سبزی</p>
+                <p class="label-food-price">قیمت: ۱۳۸،۰۰۰ تومان</p>
+            </div>
+        </div>
+
+        <!-- Single Instagram -->
+        <div class="single-instagram">
+            <img src="img/bg-img/insta7.jpg" alt="">
+            <!-- Image Zoom -->
+            <a href="img/bg-img/insta7.jpg" class="img-zoom" title="پست اینستاگرام">+</a>
+            <div class="label-food">
+                <p class="label-food-name">قرمه سبزی</p>
+                <p class="label-food-price">قیمت: ۱۳۸،۰۰۰ تومان</p>
+            </div>
+        </div>
+
+        <!-- Single Instagram -->
+        <div class="single-instagram">
+            <img src="img/bg-img/insta8.jpg" alt="">
+            <!-- Image Zoom -->
+            <a href="img/bg-img/insta8.jpg" class="img-zoom" title="پست اینستاگرام">+</a>
+            <div class="label-food">
+                <p class="label-food-name">قرمه سبزی</p>
+                <p class="label-food-price">قیمت: ۱۳۸،۰۰۰ تومان</p>
+            </div>
+        </div>
+
+        <!-- Single Instagram -->
+        <div class="single-instagram">
+            <img src="img/bg-img/insta9.jpg" alt="">
+            <!-- Image Zoom -->
+            <a href="img/bg-img/insta9.jpg" class="img-zoom" title="پست اینستاگرام">+</a>
+            <div class="label-food">
+                <p class="label-food-name">قرمه سبزی</p>
+                <p class="label-food-price">قیمت: ۱۳۸،۰۰۰ تومان</p>
+            </div>
+        </div>
+
+        <!-- Single Instagram -->
+        <div class="single-instagram">
+            <img src="img/bg-img/insta10.jpg" alt="">
+            <!-- Image Zoom -->
+            <a href="img/bg-img/insta10.jpg" class="img-zoom" title="پست اینستاگرام">+</a>
+        </div>
+    </div>
+    </div>
+    <div class="tab-pane fade" id="menu-farangi" role="tabpanel" aria-labelledby="menu-farangi-tab">
+        <div class="menu-area-resturan d-flex flex-wrap">
+            <!-- Single Instagram -->
+            <div class="single-instagram">
+                <div class="content-food">
+                    <img src="img/bg-img/insta1.jpg" alt="">
+                    <!-- Image Zoom -->
+                    <a href="img/bg-img/insta1.jpg" class="img-zoom" title="پست اینستاگرام">+</a>
+                </div>
+                <div class="label-food">
+                    <p class="label-food-name">قرمه سبزی</p>
+                    <p class="label-food-price">قیمت: ۱۳۸،۰۰۰ تومان</p>
+                </div>
+            </div>
+
+            <!-- Single Instagram -->
+            <div class="single-instagram">
+                <img src="img/bg-img/insta10.jpg" alt="">
+                <!-- Image Zoom -->
+                <a href="img/bg-img/insta10.jpg" class="img-zoom" title="پست اینستاگرام">+</a>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Instagram Area End ##### -->
+
+    <!-- ##### Hero Area Start ##### -->
+    <div class="hero-area">
+        <!-- Hero Post Slides -->
+        <div class="hero-post-slides owl-carousel">
+            <!-- Single Slide -->
+            <div class="single-slide">
+                <!-- Blog Thumbnail -->
+                <div class="blog-thumbnail">
+                    <a href="#"><img src="{{ asset('img/bg-img/1.jpg') }}" alt=""></a>
+                </div>
+
+                <!-- Blog Content -->
+                <div class="blog-content-bg">
+                    <div class="blog-content">
+                        <a href="#" class="post-tag">غذای سلامتی</a>
+                        <a href="#" class="post-title">سالاد مرغ</a>
+                        <div class="post-meta">
+                            <a href="#" class="post-date">22 آبان 1398</a>
+                            <a href="#" class="post-author">توسط مصطفی کلانتری فرد</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Slide -->
+            <div class="single-slide">
+                <!-- Blog Thumbnail -->
+                <div class="blog-thumbnail">
+                    <a href="#"><img src="img/bg-img/2.jpg" alt=""></a>
+                </div>
+
+                <!-- Blog Content -->
+                <div class="blog-content-bg">
+                    <div class="blog-content">
+                        <a href="#" class="post-tag">غذای سلامتی</a>
+                        <a href="#" class="post-title">سالاد مرغ</a>
+                        <div class="post-meta">
+                            <a href="#" class="post-date">22 آبان 1398</a>
+                            <a href="#" class="post-author">توسط مصطفی کلانتری فرد</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Slide -->
+            <div class="single-slide">
+                <!-- Blog Thumbnail -->
+                <div class="blog-thumbnail">
+                    <a href="#"><img src="img/bg-img/3.jpg" alt=""></a>
+                </div>
+
+                <!-- Blog Content -->
+                <div class="blog-content-bg">
+                    <div class="blog-content">
+                        <a href="#" class="post-tag">غذای سلامتی</a>
+                        <a href="#" class="post-title">سالاد مرغ</a>
+                        <div class="post-meta">
+                            <a href="#" class="post-date">22 آبان 1398</a>
+                            <a href="#" class="post-author">توسط مصطفی کلانتری فرد</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Slide -->
+            <div class="single-slide">
+                <!-- Blog Thumbnail -->
+                <div class="blog-thumbnail">
+                    <a href="#"><img src="img/bg-img/3.jpg" alt=""></a>
+                </div>
+
+                <!-- Blog Content -->
+                <div class="blog-content-bg">
+                    <div class="blog-content">
+                        <a href="#" class="post-tag">غذای سلامتی</a>
+                        <a href="#" class="post-title">سالاد مرغ</a>
+                        <div class="post-meta">
+                            <a href="#" class="post-date">22 آبان 1398</a>
+                            <a href="#" class="post-author">توسط مصطفی کلانتری فرد</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Hero Area End ##### -->
+
+    <!-- ##### Catagory Area Start ##### -->
+    <div class="post-catagory section-padding-100-0 mb-70">
+        <div class="container">
+            <div class="row justify-content-center">
+                <!-- Single Post Catagory -->
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-post-catagory mb-30">
+                        <img src="img/bg-img/4.jpg" alt="">
+                        <!-- Content -->
+                        <div class="catagory-content-bg">
+                            <div class="catagory-content">
+                                <a href="#" class="post-tag">بهترین</a>
+                                <a href="#" class="post-title">غذای سلامتی</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Post Catagory -->
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-post-catagory mb-30">
+                        <img src="img/bg-img/5.jpg" alt="">
+                        <!-- Content -->
+                        <div class="catagory-content-bg">
+                            <div class="catagory-content">
+                                <a href="#" class="post-tag">بهترین</a>
+                                <a href="#" class="post-title">غذاهای ارگانیک</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Post Catagory -->
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-post-catagory mb-30">
+                        <img src="img/bg-img/6.jpg" alt="">
+                        <!-- Content -->
+                        <div class="catagory-content-bg">
+                            <div class="catagory-content">
+                                <a href="#" class="post-tag">بهترین</a>
+                                <a href="#" class="post-title">غذای سبزیجات</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Catagory Area End ##### -->
+    <script src="{{ asset('js/menu-tab.js') }}"></script>
+@endsection
