8d206f4ccfe536169f92ef8b529f3ca423131f4c
Reza Hamidpour <hamidpourr@gmail.com>
2022-02-02 22:14:18 +0330

    routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,5 +17,26 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+
+Route::middleware('auth')->namespace('App\Http\Controllers\Admin')->name('admin.')->prefix('/admin')->group(function () {
+    Route::get('/', function(){
+        return view('admin.layouts.food.create');
+//        return view('admin.layouts.dashboard');
+    })->name('admin-dashboard');
+    Route::get('/', function(){
+        return view('admin.layouts.dashboard');
+    })->name('admin-dashboard');
+    Route::get('/food', function(){
+        return view('admin.layouts.food.create');
+    })->name('foods');
+    Route::get('/category', function(){
+        return view('admin.layouts.category.create');
+    })->name('category');
+    Route::get('/category/list', function(){
+        return view('admin.layouts.category.list');
+    })->name('category-list');
+
+});
+
 
 
